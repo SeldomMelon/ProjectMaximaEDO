2950f0cc1b35880ac503427804227acaf022fa11
timurtm72 <timur.sultanov1972@gmail.com>
2023-05-07 13:23:44 +0300

    Revert "Developer"

--- src/main/java/ru/maxima/projectmaximaedo/repository/DocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.maxima.projectmaximaedo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.maxima.projectmaximaedo.models.Document;
-
-public interface DocumentRepository extends JpaRepository<Document,Long> {
-}
